c98defa4df94429b1bcf78e8ddffe89083bc4ff1
Maria Campbell <interglobalmedia@gmail.com>
2022-04-17 09:21:10 -0400

    Add searchInputField state prop and handler

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     super()
     this.state = {
       monsters: [],
+      searchInputField: ''
     }
+    console.log('constructor')
   }
 
   async componentDidMount() {
@@ -25,12 +27,30 @@ class App extends Component {
     } catch (error) {
       console.log(error)
     }
+    console.log(`componentDidMount`)
+  }
+
+  searchInputChangeHandler = (event) => {
+    console.log(event)
+    event.preventDefault()
+    this.setState(() => {
+      return {
+        searchInputField: event.target.value
+      }
+    }, () => {
+      console.log(this.state)
+    })  
   }
  
   render() {
-    const {monsters} = this.state
+    console.log(`render`)
+    const { monsters, searchInputField } = this.state
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchInputField.toLowerCase()))
+    console.log(filteredMonsters)
     return (
       <div className="App">
+        <input className="search-box" type="search" placeholder="search monsters" onChange={this.searchInputChangeHandler} />
         {monsters.map((monster) => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
